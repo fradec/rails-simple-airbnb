ee1d2dd3f184cd7f9deecf3a34ce5e7b0364dc49
Arnaud de Castelbajac <adecastelbajac@gmx.com>
2019-08-14 16:46:23 +0200

    setup css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats…'
+flats_attributes = [
+  {
+    name:         'Dish room',
+    address:      '7 Boundary St, London E2 7JE',
+    description:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name:         'Mid East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name:         'Full North',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    price_per_night: 45,
+    number_of_guests: 6
+  },
+  {
+    name:         'Le Wagon',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    description:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    price_per_night: 7,
+    number_of_guests: 45
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
